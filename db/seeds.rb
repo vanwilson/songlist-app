44027a3b3f8024b85db294dda9fe250567be7de8
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-26 12:28:19 -0500

    added 5 songs to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# song = Song.new(title: "This is Me", album: "The Greatest Showman Soundtrack", artist: "Kaela Settle", year: 2017)
+# song.save
+song = Song.new(title: "Glorious Day", album: "Worthy of Your Name", artist: "Passion & Kristian Stanfill", year: 2017)
+song.save
+song = Song.new(title: "Here Comes the Sun", album: "Abbey Road", artist: "The Beatles", year: 1969)
+song.save
+song = Song.new(title: "Good God Almighty", album: "Good God Almighty", artist: "Crowder", year: 2021)
+song.save
+song = Song.new(title: "9 to 5", album: "Ultimate Dolly Parton", artist: "Dolly Parton", year: 1980)
+song.save
